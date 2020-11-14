ea71765c8a98a355c0c5a31bf6c2c48907cc6a1f
Len Payne <len.payne@gmail.com>
2020-11-13 22:30:39 -0500

    Initial Attempts at Cobra Command

---
+++ cli/cmd/root.go
@@ -0,0 +1,62 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+var rootCmd = &cobra.Command{
+	Use:   "battlesnake",
+	Short: "Battlesnake Command-Line Interface",
+	Long:  "Tools and utilities for Battlesnake games.",
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.battlesnake.yaml)")
+
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".battlesnake" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".battlesnake")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/BattlesnakeOfficial/rules
 
 go 1.13
 
-require github.com/stretchr/testify v1.4.0
+require (
+	github.com/google/uuid v1.1.2
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/spf13/cobra v1.1.1
+	github.com/spf13/viper v1.7.1
+	github.com/stretchr/testify v1.4.0
+)
