79d973220c20a85bdaa901a14d1db0b4c85d70be
Rob O'Dwyer <odwyerrob@gmail.com>
2021-12-17 13:57:43 -0800

    convert solo to pipeline stage

--- solo.go
+++ solo.go
@@ -7,6 +7,10 @@ type SoloRuleset struct {
 func (r *SoloRuleset) Name() string { return "solo" }
 
 func (r *SoloRuleset) IsGameOver(b *BoardState) (bool, error) {
+	return r.callStageFunc(GameOverSolo, b, []SnakeMove{})
+}
+
+func GameOverSolo(b *BoardState, settings RulesetSettings, moves []SnakeMove) (bool, error) {
 	for i := 0; i < len(b.Snakes); i++ {
 		if b.Snakes[i].EliminatedCause == NotEliminated {
 			return false, nil
