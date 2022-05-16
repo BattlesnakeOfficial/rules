2cdd5ff75fbd83f31e05871e0442095dbbc96a7f
Rob O'Dwyer <odwyerrob@gmail.com>
2022-05-16 16:08:02 -0700

    add empty and royale maps

---
+++ maps/empty_test.go
@@ -0,0 +1,164 @@
+package maps
+
+import (
+	"testing"
+
+	"github.com/BattlesnakeOfficial/rules"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEmptyMapInterface(t *testing.T) {
+	var _ GameMap = EmptyMap{}
+}
+
+func TestEmptyMapSetupBoard(t *testing.T) {
+	m := EmptyMap{}
+	settings := rules.Settings{}
+
+	tests := []struct {
+		name              string
+		initialBoardState *rules.BoardState
+		rand              rules.Rand
+
+		expected *rules.BoardState
+		err      error
+	}{
+		{
+			"empty 7x7",
+			rules.NewBoardState(7, 7),
+			rules.MinRand,
+			&rules.BoardState{
+				Width:   7,
+				Height:  7,
+				Snakes:  []rules.Snake{},
+				Food:    []rules.Point{},
+				Hazards: []rules.Point{},
+			},
+			nil,
+		},
+		{
+			"not enough room for snakes 7x7",
+			&rules.BoardState{
+				Width:   7,
+				Height:  7,
+				Snakes:  generateSnakes(9),
+				Food:    []rules.Point{},
+				Hazards: []rules.Point{},
+			},
+			rules.MinRand,
+			nil,
+			rules.ErrorTooManySnakes,
+		},
+		{
+			"not enough room for snakes 5x5",
+			&rules.BoardState{
+				Width:   5,
+				Height:  5,
+				Snakes:  generateSnakes(14),
+				Food:    []rules.Point{},
+				Hazards: []rules.Point{},
+			},
+			rules.MinRand,
+			nil,
+			rules.ErrorNoRoomForSnake,
+		},
+		{
+			"full 11x11 min",
+			&rules.BoardState{
+				Width:   11,
+				Height:  11,
+				Snakes:  generateSnakes(8),
+				Food:    []rules.Point{},
+				Hazards: []rules.Point{},
+			},
+			rules.MinRand,
+			&rules.BoardState{
+				Width:  11,
+				Height: 11,
+				Snakes: []rules.Snake{
+					{ID: "1", Body: []rules.Point{{X: 1, Y: 1}, {X: 1, Y: 1}, {X: 1, Y: 1}}, Health: 100},
+					{ID: "2", Body: []rules.Point{{X: 1, Y: 5}, {X: 1, Y: 5}, {X: 1, Y: 5}}, Health: 100},
+					{ID: "3", Body: []rules.Point{{X: 1, Y: 9}, {X: 1, Y: 9}, {X: 1, Y: 9}}, Health: 100},
+					{ID: "4", Body: []rules.Point{{X: 5, Y: 1}, {X: 5, Y: 1}, {X: 5, Y: 1}}, Health: 100},
+					{ID: "5", Body: []rules.Point{{X: 5, Y: 9}, {X: 5, Y: 9}, {X: 5, Y: 9}}, Health: 100},
+					{ID: "6", Body: []rules.Point{{X: 9, Y: 1}, {X: 9, Y: 1}, {X: 9, Y: 1}}, Health: 100},
+					{ID: "7", Body: []rules.Point{{X: 9, Y: 5}, {X: 9, Y: 5}, {X: 9, Y: 5}}, Health: 100},
+					{ID: "8", Body: []rules.Point{{X: 9, Y: 9}, {X: 9, Y: 9}, {X: 9, Y: 9}}, Health: 100},
+				},
+				Food:    []rules.Point{},
+				Hazards: []rules.Point{},
+			},
+			nil,
+		},
+		{
+			"full 11x11 max",
+			&rules.BoardState{
+				Width:   11,
+				Height:  11,
+				Snakes:  generateSnakes(8),
+				Food:    []rules.Point{},
+				Hazards: []rules.Point{},
+			},
+			rules.MaxRand,
+			&rules.BoardState{
+				Width:  11,
+				Height: 11,
+				Snakes: []rules.Snake{
+					{ID: "1", Body: []rules.Point{{X: 1, Y: 5}, {X: 1, Y: 5}, {X: 1, Y: 5}}, Health: 100},
+					{ID: "2", Body: []rules.Point{{X: 1, Y: 9}, {X: 1, Y: 9}, {X: 1, Y: 9}}, Health: 100},
+					{ID: "3", Body: []rules.Point{{X: 5, Y: 1}, {X: 5, Y: 1}, {X: 5, Y: 1}}, Health: 100},
+					{ID: "4", Body: []rules.Point{{X: 5, Y: 9}, {X: 5, Y: 9}, {X: 5, Y: 9}}, Health: 100},
+					{ID: "5", Body: []rules.Point{{X: 9, Y: 1}, {X: 9, Y: 1}, {X: 9, Y: 1}}, Health: 100},
+					{ID: "6", Body: []rules.Point{{X: 9, Y: 5}, {X: 9, Y: 5}, {X: 9, Y: 5}}, Health: 100},
+					{ID: "7", Body: []rules.Point{{X: 9, Y: 9}, {X: 9, Y: 9}, {X: 9, Y: 9}}, Health: 100},
+					{ID: "8", Body: []rules.Point{{X: 1, Y: 1}, {X: 1, Y: 1}, {X: 1, Y: 1}}, Health: 100},
+				},
+				Food:    []rules.Point{},
+				Hazards: []rules.Point{},
+			},
+			nil,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			nextBoardState := rules.NewBoardState(test.initialBoardState.Width, test.initialBoardState.Height)
+			editor := NewBoardStateEditor(nextBoardState)
+			settings := settings.WithRand(test.rand)
+
+			err := m.SetupBoard(test.initialBoardState, settings, editor)
+
+			if test.err != nil {
+				require.Equal(t, test.err, err)
+			} else {
+				require.Equal(t, test.expected, nextBoardState)
+			}
+		})
+	}
+}
+
+func TestEmptyMapUpdateBoard(t *testing.T) {
+	m := EmptyMap{}
+	initialBoardState := &rules.BoardState{
+		Width:   2,
+		Height:  2,
+		Snakes:  []rules.Snake{},
+		Food:    []rules.Point{{X: 0, Y: 0}},
+		Hazards: []rules.Point{},
+	}
+	settings := rules.Settings{
+		FoodSpawnChance: 50,
+		MinimumFood:     2,
+	}.WithRand(rules.MaxRand)
+	nextBoardState := initialBoardState.Clone()
+
+	err := m.UpdateBoard(initialBoardState.Clone(), settings, NewBoardStateEditor(nextBoardState))
+
+	require.NoError(t, err)
+	require.Equal(t, &rules.BoardState{
+		Width:   2,
+		Height:  2,
+		Snakes:  []rules.Snake{},
+		Food:    []rules.Point{{X: 0, Y: 0}},
+		Hazards: []rules.Point{},
+	}, nextBoardState)
+}
---
+++ maps/royale.go
@@ -0,0 +1,74 @@
+package maps
+
+import (
+	"errors"
+
+	"github.com/BattlesnakeOfficial/rules"
+)
+
+type RoyaleHazardsMap struct{}
+
+func init() {
+	globalRegistry.RegisterMap("royale", RoyaleHazardsMap{})
+}
+
+func (m RoyaleHazardsMap) ID() string {
+	return "royale"
+}
+
+func (m RoyaleHazardsMap) Meta() Metadata {
+	return Metadata{
+		Name:        "Royale",
+		Description: "A map where hazards are generated every N turns",
+		Author:      "Battlesnake",
+	}
+}
+
+func (m RoyaleHazardsMap) SetupBoard(lastBoardState *rules.BoardState, settings rules.Settings, editor Editor) error {
+	return StandardMap{}.SetupBoard(lastBoardState, settings, editor)
+}
+
+func (m RoyaleHazardsMap) UpdateBoard(lastBoardState *rules.BoardState, settings rules.Settings, editor Editor) error {
+	// Royale uses the current turn to generate hazards, not the previous turn that's in the board state
+	turn := lastBoardState.Turn + 1
+
+	if settings.RoyaleSettings.ShrinkEveryNTurns < 1 {
+		return errors.New("royale game can't shrink more frequently than every turn")
+	}
+
+	if turn < settings.RoyaleSettings.ShrinkEveryNTurns {
+		return nil
+	}
+
+	// Reset hazards every turn and re-generate them
+	editor.ClearHazards()
+
+	// Get random generator for turn zero, because we're regenerating all hazards every time.
+	randGenerator := settings.GetRand(0)
+
+	numShrinks := turn / settings.RoyaleSettings.ShrinkEveryNTurns
+	minX, maxX := int32(0), lastBoardState.Width-1
+	minY, maxY := int32(0), lastBoardState.Height-1
+	for i := int32(0); i < numShrinks; i++ {
+		switch randGenerator.Intn(4) {
+		case 0:
+			minX += 1
+		case 1:
+			maxX -= 1
+		case 2:
+			minY += 1
+		case 3:
+			maxY -= 1
+		}
+	}
+
+	for x := int32(0); x < lastBoardState.Width; x++ {
+		for y := int32(0); y < lastBoardState.Height; y++ {
+			if x < minX || x > maxX || y < minY || y > maxY {
+				editor.AddHazard(rules.Point{X: x, Y: y})
+			}
+		}
+	}
+
+	return nil
+}
