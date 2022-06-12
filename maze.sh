ab2a112ad65ab1196dc427729c46bf31f5c2ce19
Corey Alexander <coreyja@gmail.com>
2022-06-12 14:48:23 -0400

    Remove my little debugging script that I added

--- maze.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-go build -o battlesnake ./cli/battlesnake/main.go
-./battlesnake play -W 10 -H 11 --name Frank --url http://0:3000/famished-frank --gametype solo -v -d 20 --map coreyja_maze --hazardDamagePerTurn 100
