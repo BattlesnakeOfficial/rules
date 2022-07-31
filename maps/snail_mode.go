ad38a128298497f1e5c9f22c73d1d6e5be067423
Corey Alexander <coreyja@gmail.com>
2022-07-31 16:48:53 -0400

    Update from Stream July 31
    Added comments to most functions and important bits of code
    
    Also changed the map so that instead of a fixed number of 7 hazards,
    we add hazards equal to the length of the snake.

--- maps/snail_mode.go
+++ maps/snail_mode.go
@@ -6,17 +6,20 @@ import (
 
 type SnailModeMap struct{}
 
+// init registers this map in the global registry.
 func init() {
 	globalRegistry.RegisterMap("snail_mode", SnailModeMap{})
 }
 
+// ID returns a unique identifier for this map.
 func (m SnailModeMap) ID() string {
 	return "snail_mode"
 }
 
+// Meta returns the non-functional metadata about this map.
 func (m SnailModeMap) Meta() Metadata {
 	return Metadata{
-		Name:        "snail_mode",
+		Name:        "Snail Mode",
 		Description: "Snakes leave behind a trail of hazards",
 		Author:      "coreyja and jlafayette",
 		Version:     1,
@@ -26,6 +29,7 @@ func (m SnailModeMap) Meta() Metadata {
 	}
 }
 
+// SetupBoard here is pretty 'standard' and doesn't do any special setup for this game mode
 func (m SnailModeMap) SetupBoard(initialBoardState *rules.BoardState, settings rules.Settings, editor Editor) error {
 	rand := settings.GetRand(0)
 
@@ -52,10 +56,16 @@ func (m SnailModeMap) SetupBoard(initialBoardState *rules.BoardState, settings r
 	return nil
 }
 
+// hash takes in a Point and the height of the board, and returns a unique integer to be used in maps
+// Convert the given point into an integer that can be used as a map key
+// This is takes the X and Y coordinates and combines them into a single integer
+// that uniquely identifies the point given the current board size
 func hash(point rules.Point, height int) int {
 	return point.X + point.Y*height
 }
 
+// dehash converts an integer back onto a Point.
+// Storing points as int allows using them as a map key.
 func dehash(hash int, height int) rules.Point {
 	x := hash % height
 	y := hash / height
@@ -63,39 +73,53 @@ func dehash(hash int, height int) rules.Point {
 	return rules.Point{X: x, Y: y}
 }
 
+// storeTailLocation returns an offboard point that corresponds to the given point.
+// This is useful for storing state that can be accessed next turn.
 func storeTailLocation(point rules.Point, height int) rules.Point {
 	return rules.Point{X: point.X, Y: point.Y + height}
 }
 
+// getPrevTailLocation returns the onboard point that corresponds to an offboard point.
+// This is useful for restoring state that was stored last turn.
 func getPrevTailLocation(point rules.Point, height int) rules.Point {
 	return rules.Point{X: point.X, Y: point.Y - height}
 }
 
+// outOfBounds determines if the given point is out of bounds for the current board size
 func outOfBounds(p rules.Point, w, h int) bool {
 	return p.X < 0 || p.Y < 0 || p.X >= w || p.Y >= h
 }
 
+// doubleTail determine if the snake has a double stacked tail currently
 func doubleTail(snake *rules.Snake) bool {
 	almostTail := snake.Body[len(snake.Body)-2]
 	tail := snake.Body[len(snake.Body)-1]
 	return almostTail.X == tail.X && almostTail.Y == tail.Y
 }
 
+// isEliminated determines if the snake is already eliminated
 func isEliminated(s *rules.Snake) bool {
 	return s.EliminatedCause != rules.NotEliminated
 }
 
+// UpdateBoard does the work of placing the hazards along the 'snail tail' of snakes
+// This is responsible for saving the current tail location off the board
+// and restoring the previous tail position. This also handles removing one hazards from
+// the current stacks so the hazards tails fade as the snake moves away.
 func (m SnailModeMap) UpdateBoard(lastBoardState *rules.BoardState, settings rules.Settings, editor Editor) error {
 	err := StandardMap{}.UpdateBoard(lastBoardState, settings, editor)
 	if err != nil {
 		return err
 	}
 
-	// store hazards so we can decrement stack of hazards by 1
-	// we can use the lastBoardState
-
+	// This map decrements the stack of hazards on a point each turn, so they
+	// need to be cleared first.
 	editor.ClearHazards()
 
+	// This is a list of all the hazards we want to add for the previous tails
+	// These were stored off board in the previous turn as a way to save state
+	// When we add the locations to this list we have already converted the off-board
+	// points to on-board points
 	tailLocations := make([]rules.Point, 0, len(lastBoardState.Snakes))
 
 	// Count the number of hazards for a given position
@@ -112,7 +136,8 @@ func (m SnailModeMap) UpdateBoard(lastBoardState *rules.BoardState, settings rul
 		}
 	}
 
-	// Add back existing hazards, but with a stack of 1 less than before
+	// Add back existing hazards, but with a stack of 1 less than before.
+	// This has the effect of making the snail-trail disappear over time.
 	for hazardHash, count := range hazardCounts {
 		hazard := dehash(hazardHash, lastBoardState.Height)
 
@@ -121,7 +146,9 @@ func (m SnailModeMap) UpdateBoard(lastBoardState *rules.BoardState, settings rul
 		}
 	}
 
-	// ensure stack of 7 for tail segment of each snake
+	// Store a stack of hazards for the tail of each snake.  This is stored out
+	// of bounds and then applied on the next turn.  The stack count is equal
+	// the lenght of the snake.
 	for _, snake := range lastBoardState.Snakes {
 		if isEliminated(&snake) {
 			continue
@@ -135,13 +162,18 @@ func (m SnailModeMap) UpdateBoard(lastBoardState *rules.BoardState, settings rul
 
 		tail := snake.Body[len(snake.Body)-1]
 		offBoardTail := storeTailLocation(tail, lastBoardState.Height)
-		editor.AddHazard(offBoardTail)
+		for i := 0; i < len(snake.Body); i++ {
+			editor.AddHazard(offBoardTail)
+		}
 	}
 
-	// read offboard tails and apply 7 hazards
+	// Read offboard tails and move them to the board. The offboard tails are
+	// stacked based on the length of the snake
 	for _, p := range tailLocations {
 
 		// Skip position if a snakes head occupies it.
+		// Otherwise hazard shows up in the viewer on top of a snake head, but
+		// does not damage the snake, which is visually confusing.
 		isHead := false
 		for _, snake := range lastBoardState.Snakes {
 			if isEliminated(&snake) {
@@ -157,10 +189,7 @@ func (m SnailModeMap) UpdateBoard(lastBoardState *rules.BoardState, settings rul
 			continue
 		}
 
-		currentCount := hazardCounts[hash(p, lastBoardState.Height)]
-		for i := 0; i < 7-currentCount; i++ {
-			editor.AddHazard(p)
-		}
+		editor.AddHazard(p)
 	}
 
 	return nil
